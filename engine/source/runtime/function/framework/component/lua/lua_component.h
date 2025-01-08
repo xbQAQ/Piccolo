7163d89ca3f45ce4d301ed5f623d2a76a6095569
Interesting <1216744673@qq.com>
2025-01-08 17:52:06 +0800

    Section 1: Code Struct

--- engine/source/runtime/function/framework/component/lua/lua_component.h
+++ engine/source/runtime/function/framework/component/lua/lua_component.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "sol/sol.hpp"
 #include "runtime/function/framework/component/component.h"
+#include "sol/sol.hpp"
 
 namespace Piccolo
 {
@@ -15,14 +15,6 @@ namespace Piccolo
         void postLoadResource(std::weak_ptr<GObject> parent_object) override;
 
         void tick(float delta_time) override;
-
-        template<typename T>
-        static void set(std::weak_ptr<GObject> game_object, const char* name, T value);
-
-        template<typename T>
-        static T get(std::weak_ptr<GObject> game_object, const char* name);
-
-        static void invoke(std::weak_ptr<GObject> game_object, const char* name);
     protected:
         sol::state m_lua_state;
         META(Enable)
